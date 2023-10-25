5e66f48957b80cac3e4e873864130ef9031bab1c
Pax-Newman <newmanp@wwu.edu>
2023-10-24 18:52:07 -0700

    added setup and init for package

---
+++ Topic Clustering/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="topic_clustering",
+    version="1.0.0",
+    author="Pax Newman",
+    author_email="pax.newman@email.com",
+    description="A cli tool and api for clustering documents by topic.",
+    url="https://github.com/Pax-Newman/DS-Toys/tree/main/Topic%20Clustering",
+    license="MIT",
+    install_requires=[
+        "sentence_transformers",
+        "numpy",
+        "pyyaml",
+        "typer",
+        "pandas",
+    ],
+    entry_points={
+        "console_scripts": [
+            "topic_cluster = topic_clustering.main:app",
+        ],
+    },
+    include_package_data=True,
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)
